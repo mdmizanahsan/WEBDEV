958abe4f74aff035c19c4d369a4f5b85a838c927
mdmizanahsan <work.mdmizanahsan@gmail.com>
2025-06-14 02:14:24 +0530

    Basic Question1 of String

---
+++ Backend/Java/String/Question1.java
@@ -0,0 +1,10 @@
+package Backend.Java.String;
+
+public class Question1 {
+    public static void main(String[] args) {
+        String s1 = "MY naMe is MiZAn";
+        System.out.println(s1);
+        System.out.println(s1.toLowerCase());
+        System.out.println(s1.toUpperCase()); 
+    }
+}
