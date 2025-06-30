c18d37d5411ee641908cfeef5ebeaf0dcdd1024d
mdmizanahsan <work.mdmizanahsan@gmail.com>
2025-06-30 15:35:50 +0530

    Basic of js

---
+++ Frontend/JS/Day:-2/app.js
@@ -0,0 +1,24 @@
+    console.log("**********Example of Arithmetic Operators**********");
+let a=5;
+let b=4;
+console.log("Sum of two number is =",a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+   console.log("************Example of Unary Operators****************");
+   let a1= 10;
+   let b1= 10;
+   console.log(a1++);  // 10
+   console.log(++a1);  // 12
+
+    console.log("************Example of Assignment Operators****************");
+    let a2 = 20;
+    let b2 = 30;
+    a2 = b2;
+    console.log(a2);
+
+       console.log("************Example of Comparison Operators****************");
+
