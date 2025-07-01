1a927606dccee96e8d5bfd323f01eb7ed1e12781
mdmizanahsan <work.mdmizanahsan@gmail.com>
2025-07-01 12:27:40 +0530

    Controled sta in js

---
+++ Frontend/JS/Day:-4/app.js
@@ -0,0 +1,32 @@
+console.log("***********for loop*************");
+
+for(let i=1; i<= 5;i++){
+    console.log(i);
+}
+ 
+// print odd number
+ console.log("******* print odd number *******");
+
+ for(let i=1; i<=15; i=i+2){
+    console.log(i);
+ }
+ 
+ console.log("*********While loop**********");
+  // print number
+  let i =1;
+  while(i <= 10){
+    console.log(i);
+    i++;
+  }
+  // question 
+    let fav = "ironman";
+      let guess = prompt("guess my fav movie");
+      while((guess != fav) && (guess != "quit")){
+        console.log("Wrong");
+      guess = prompt("Please try again");
+      }
+       if(guess == fav){
+        console.log("congrats");
+       } else{
+        console.log("you quit");
+       }
