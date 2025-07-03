d39e76962e2369c7e80e151a7f0afce305964389
mdmizanahsan <work.mdmizanahsan@gmail.com>
2025-07-03 18:33:44 +0530

    Question

---
+++ Frontend/JS/Day:-7/forEach/app.js
@@ -0,0 +1,17 @@
+   let arr = [1,2,3,4,5];
+
+    let print = function(el) {
+        console.log(el);
+   }
+
+     // call
+     arr.forEach(print);  // 
+
+            // or
+        
+     let arr1 = [1,2,3,5,4];
+
+       arr.forEach((el) => {
+        console.log(el);
+       }
+    );
