5bea02434a2f30f668cf7f1c275b4b76e07eebd4
mdmizanahsan <work.mdmizanahsan@gmail.com>
2025-07-05 21:30:11 +0530

    DOM

---
+++ Frontend/JS/Day:-8/DOM.Event/onclick/app.js
@@ -0,0 +1,25 @@
+
+let btn = document.querySelector("button");
+console.dir(btn);
+
+   btn.onclick = function() {
+    console.log("button was clicked");
+   }
+
+
+   // function sayhello() {
+   //  alert("hello");
+   //}
+
+   //  btn.onclick = sayhello;
+
+   // i can set for multiple button
+
+   //  let btns = document.querySelectorAll("button");
+
+   //   har button par janey ke liye loop laga rhe hai
+   //   for(btn of btns){
+   //    btn.onclick = sayhello;
+   //    }
+
+   // os ke baad function bna denge pichle baar ke tra
