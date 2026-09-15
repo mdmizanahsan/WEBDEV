223ae9c9c3b5ef3b638a2f836c7ffa4f9ee068f7
Md Mizan Ahsan <work.mdmizanahsan@gmail.com>
2026-09-15 22:31:45 +0530

    Practice Question

---
+++ Backend/Java/BasicQuestion/SwapNumber.java
@@ -0,0 +1,18 @@
+package Backend.Java.BasicQuestion;
+
+public class SwapNumber {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        int c = 0;
+
+        c = a ;
+        a = b ;
+        b = c ;
+
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+
+    }
+}
